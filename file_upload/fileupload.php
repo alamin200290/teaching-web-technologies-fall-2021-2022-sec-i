47662ea2e1986314b60187516e1db769bac1faf1
sarwar_sikder <amin.se.aiub@gmail.com>
2021-10-19 16:17:55 +0600

    file upload

---
+++ file_upload/fileupload.php
@@ -0,0 +1,16 @@
+<?php 
+
+	//print_r($_FILES['myfile']);
+	$des = "upload/".$_FILES["myfile"]["name"];
+
+
+	//echo $_FILES["myfile"]["tmp_name"];
+	//echo $des;
+	$src = $_FILES["myfile"]["tmp_name"];
+
+	if(move_uploaded_file($src, $des)){
+		echo "Done";
+	}else{
+		echo "Error";
+	}
+?>
